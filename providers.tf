7f3b00735061fc700d85591cae958cecb7d8fafe
Hoa, Tran Van <hoatrancntt@gmail.com>
2026-03-25 11:22:35 +0700

    Add SSH settings to Proxmox provider configuration
    Added SSH configuration for Proxmox provider.

--- providers.tf
+++ providers.tf
@@ -19,4 +19,14 @@ provider "proxmox" {
   endpoint  = var.proxmox_api_url
   api_token = var.proxmox_api_token
   insecure  = true
+  ssh {
+    agent    = false
+    username = "root"
+    password = var.proxmox_ssh_password
+
+    node {
+      name    = var.proxmox_node       # "promox02"
+      address = var.proxmox_ssh_host   # "172.199.10.165"
+    }
+  }
 }
